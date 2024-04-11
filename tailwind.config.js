d6e87ea2d0f220bc2f9a9d7c00a297c8e63cf464
Rene D Perez <reneperez@renpen-Mac.local>
2024-04-10 22:07:28 -0700

    I worked on the onnit section

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('https://images.ctfassets.net/nfc1w9qpk6sl/3jyLsbjyYsheLrUEwI64Hh/cfc525ee3e41e0c993865c292c400d18/What-is-Onnit-Banner-2.jpg?q=40&fm=jpg&fl=progressive')"
+        // 'footer-texture': "url('/img/footer-texture.png')",
+      }
+    }
+  }
+}
