83cee37c635383d2f122e0fd59609af9020baab2
RenPen3 <wrenwiththepen@gmail.com>
2024-06-04 22:02:07 -0700

    I added JavaScript and made the sub menu responsive

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('Hello');
+const ham = document.getElementById('hamburger-menu');
+const subMenu = document.getElementById('sub-menu');
+const x = document.getElementById('x-btn');
+
+
+
+ham.addEventListener("click", () => {
+  subMenu.classList.toggle('show');
+  console.log(x);
+});
+
+x.addEventListener("click", () => {
+  subMenu.classList.remove('show')
+  console.log("Working");
+});
+
