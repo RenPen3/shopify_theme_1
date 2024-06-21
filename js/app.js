faaa13c75e4846af46043a303a056523d326a929
RenPen3 <wrenwiththepen@gmail.com>
2024-06-20 21:37:47 -0700

    I added menu js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 const ham = document.getElementById('hamburger-menu');
 const subMenu = document.getElementById('sub-menu');
 const x = document.getElementById('x-btn');
+const navLinks = document.querySelectorAll('.nav-link');
+// const dropDowns = document.querySelectorAll('.nav-drop');
+
+// navLinks.forEach(navLink => {
+//   navLink.addEventListener('click', () => {
+//     const dropDowns = document.querySelector('.nav-drop');
+//     dropDowns.classList.toggle('down');
+
+//     console.log(dropDowns);
+
+//   })
+// });
+
 
 
 
